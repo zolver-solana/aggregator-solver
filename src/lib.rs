f31f27653a0e133999d652a6cdb2984568118f48
imduchuyyy <duchuy.124dk@gmail.com>
2026-01-09 11:02:40 +0700

    feat(solver): basic solver

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+
+#[derive(Debug)]
+pub struct Solver {
+    name: String,
+    version: String,
+}
+
+impl Solver {
+    pub fn new(name: &str, version: &str) -> Self {
+        Solver {
+            name: name.to_string(),
+            version: version.to_string(),
+        }
+    }
+
+    pub fn solve(&self, intent: &str) -> String {
+        format!("Solving '{}' using {} v{}", intent, self.name, self.version)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use aggregator_solver::Solver;
+
 fn main() {
-    println!("Hello, world!");
+    let solver = Solver::new("AggregatorSolver", "1.0");
+    let result = solver.solve("Example Intent");
+    println!("{}", result);
 }
